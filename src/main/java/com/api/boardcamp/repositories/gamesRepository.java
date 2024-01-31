0341050369c0c65b5b2a8ebd513d9d239b4eb957
Tiago <tiagoalex0202@gmail.com>
2024-01-31 08:34:02 -0300

    games repository

---
+++ src/main/java/com/api/boardcamp/repositories/gamesRepository.java
@@ -0,0 +1,14 @@
+package com.api.boardcamp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.api.boardcamp.models.gameModel;
+
+
+@Repository
+public interface gamesRepository extends JpaRepository < gameModel, Long > {
+    
+}
