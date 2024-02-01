06935aa280056c88eaf0e0539ae04fbb63af35c4
Tiago <tiagoalex0202@gmail.com>
2024-02-01 14:56:42 -0300

    Unprocessable Entity logic

--- src/main/java/com/api/boardcamp/repositories/rentalRepository.java
+++ src/main/java/com/api/boardcamp/repositories/rentalRepository.java
@@ -1,11 +1,19 @@
 package com.api.boardcamp.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.api.boardcamp.models.customerModel;
 import com.api.boardcamp.models.rentalModel;
 
 @Repository
 public interface rentalRepository  extends JpaRepository<rentalModel, Long>{
     boolean existsById( Long id);
+    @Query(value = " SELECT * from gameId WHERE gameId= :gameId AND returnDate = null", nativeQuery = true)
+    public List<rentalModel> findByGameId (@Param("gameId") Long id);
 }
--- src/main/java/com/api/boardcamp/services/rentalService.java
+++ src/main/java/com/api/boardcamp/services/rentalService.java
@@ -10,6 +10,7 @@ import com.api.boardcamp.dtos.rentalDto;
 import com.api.boardcamp.exceptions.CustomerNotFoundException;
 import com.api.boardcamp.exceptions.GameNotFoundException;
 import com.api.boardcamp.exceptions.RentNotFoundException;
+import com.api.boardcamp.exceptions.UnprocessableEntityException;
 import com.api.boardcamp.models.customerModel;
 import com.api.boardcamp.models.gameModel;
 import com.api.boardcamp.models.rentalModel;
@@ -37,8 +38,14 @@ public class rentalService {
             gameModel game = gamesRepository.findById(dto.getGameId()).orElseThrow(() -> new GameNotFoundException("This game has not been found in the stock!"));
             customerModel customer = customerRepository.findById(dto.getCustomerId()).orElseThrow(() -> new CustomerNotFoundException("This customer is not registered in the database!"));
             Integer price = game.getPricePerDay() * dto.getDaysRented();
-            rentalModel rent = new rentalModel(dto, game, customer, price , date);
-            return rentalRepository.save(rent);
+
+            List<rentalModel> amount = rentalRepository.findByGameId(game.getId());
+            if ( amount.size() > game.getStockTotal()) {
+                throw new UnprocessableEntityException("Our stock has ran out of this game!Please, await until they're available again");
+            } else {
+                rentalModel rent = new rentalModel(dto, game, customer, price , date);
+                return rentalRepository.save(rent);
+            }
         }
 
     public List<rentalModel> findAll() {
@@ -50,33 +57,38 @@ public class rentalService {
             
             rentalModel rent = rentalRepository.findById(id).orElseThrow(() -> new RentNotFoundException("The rent register has not been found"));
 
-            LocalDate now = LocalDate.now();
-            Timestamp timestamp = Timestamp.valueOf(now.atStartOfDay());
-            Long nowMili = timestamp.getTime();
+            if (rent.getReturnDate() == null) {
+                throw new UnprocessableEntityException("This rent has already been finalized!");
+            } else {
+                LocalDate now = LocalDate.now();
+                Timestamp timestamp = Timestamp.valueOf(now.atStartOfDay());
+                Long nowMili = timestamp.getTime();
 
-            LocalDate rentDate = rent.getRentDate();
-            Timestamp timestampRent = Timestamp.valueOf(rentDate.atStartOfDay());
-            Long rentMili = timestampRent.getTime();
+                LocalDate rentDate = rent.getRentDate();
+                Timestamp timestampRent = Timestamp.valueOf(rentDate.atStartOfDay());
+                Long rentMili = timestampRent.getTime();
 
-            Integer interval = rent.getDaysRented() * 86400000;
+                Integer interval = rent.getDaysRented() * 86400000;
 
-            Long difference = nowMili - rentMili;
+                Long difference = nowMili - rentMili;
 
-            if (difference <= interval) {
-                rentalModel updated = new rentalModel(rent, now);
+                if (difference <= interval) {
+                 rentalModel updated = new rentalModel(rent, now);
                 updated.setId(id);
                 return rentalRepository.save(updated);
-            } else {
-                Long delay = difference - interval;
-                Long days = delay / 86400000;
-                Long debt = rent.getGame().getPricePerDay() * days;
+               } else {
+                  Long delay = difference - interval;
+                  Long days = delay / 86400000;
+                  Long debt = rent.getGame().getPricePerDay() * days;
 
-                rentalModel updated = new rentalModel(rent, now, debt);
-                updated.setId(id);
-                rentalModel done = rentalRepository.save(updated);
-                return (done);
+                  rentalModel updated = new rentalModel(rent, now, debt);
+                  updated.setId(id);
+                  rentalModel done = rentalRepository.save(updated);
+                  return (done);
+               }
+
             }
-
+            
         } 
     }
 
